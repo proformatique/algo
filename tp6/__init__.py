4d0df84fdcc1a012e06ff48217d212f64bcec101
MHAMEDI.L <lts.cpge@gmail.com>
2018-01-29 18:18:47 +0000

    lydex maj

--- tp6/__init__.py
+++ tp6/__init__.py
@@ -90,12 +90,12 @@ def datetuple(date: str) -> tuple:
     return datetpl
 
 
-def formater(temps: int, duree: bool) -> str:
+def formater(temps: int, duree: bool) -> tuple:
     """Formate le temps.
 
     Note
     ----
-    99mins 99s pour la durée.
+    00mins 00s pour la durée.
     00:00:00 pour le temps.
 
     Examples
@@ -107,23 +107,27 @@ def formater(temps: int, duree: bool) -> str:
     >>> formater(32165, False)
     ('08:56:05', 0)
     """
+    
     minutes = temps // 60
     secondes = temps % 60
-    msg = '%02dmin %02ds'
-    data = minutes, secondes
-    jours = (minutes // 60) // 24
-    if not duree:
-        heure = (minutes // 60) % 24
+    heures = minutes // 60
+    jours = heures // 24
+    if duree:
+        forme = '%02dmin %02ds'
+        data = minutes, secondes
+    else:
         minutes %= 60
-        data = heure, minutes, secondes
-        msg = '%02d:%02d:%02d'
-    return msg % data, jours
+        heures %= 24
+        forme = '%02d:%02d:%02d'
+        data = heures, minutes, secondes
+    return forme % data, jours
 
 
 def tempsdefin(date: str, temps: str, duree: str) -> tuple:
     """Calcule la date de fin.
 
     La date et l'heure après la conversation.
+    
     Parameters
     ----------
     date:
@@ -134,26 +138,30 @@ def tempsdefin(date: str, temps: str, duree: str) -> tuple:
         sous la forme 00min 00s.
     Returns
     -------
-    datedefin: str
+    tempsdefin: str
     jourok: int
     """
     temps = tempsensecondes(temps)
     duree = tempsensecondes(duree)
     heurefin, jourok = formater(temps + duree, False)
-    # date = datesuivante(*datetuple(date)) if jourok else date
+    # datedefin = datesuivante(*datetuple(date)) if jourok else date
     datedefin = "Le lendemain" if jourok else date
     return datedefin, heurefin
 
 
-def analyser(contenu) -> list:
+def analyser(contenu: str) -> list:
     """Analyse le contenu du fichier appels.csv.
+    
+    Retourne un tableau à 2d de tous les champs.
+    Parameters
+    ----------
+    contenu:
+        texte à analyser.
     """
     l_contenu = contenu.split('\n')
-    # titres = l_contenu[0].split(';')
-    # print(titres)
     ll_contenu = []
-    for i in range(1, len(l_contenu)):
-        appel = l_contenu[i].split(';')
+    for ligne in l_contenu:
+        appel = ligne.split(';')
         ll_contenu.append(appel)
     return ll_contenu
 
@@ -167,10 +175,15 @@ def etendre(ll_contenu: list):
         Données du fichiers apples.csv sous forme de liste de listes.
     """
     for i, ligne in enumerate(ll_contenu):
-        date, temps, duree = ligne[1:]
-        date = tempsdefin(date, temps, duree)
-        del ll_contenu[i][3]
-        ll_contenu[i].extend(date)
+        if i == 0:
+            ligne = ligne[:-1] + ['Date de fin', 'Temps de fin']
+            ll_contenu[i] = ligne
+        else:
+            date, temps, duree = ligne[1:]
+            datetpl = tempsdefin(date, temps, duree)
+            del ll_contenu[i][3]
+            ll_contenu[i].extend(datetpl)
+    return ll_contenu
 
 
 def backtotext(ll_contenu: list) -> str:
@@ -182,9 +195,11 @@ def backtotext(ll_contenu: list) -> str:
     ll_contenu:
         résultat sous forme de liste de listes
     """
+    sepchamps = ';'
+    seplignes = '\n'
     for i, ligne in enumerate(ll_contenu):
-        ll_contenu[i] = ';'.join(ligne)
-    return '\n'.join(ll_contenu)
+        ll_contenu[i] = sepchamps.join(ligne)
+    return seplignes.join(ll_contenu)
 
 
 if __name__ == '__main__':
@@ -193,8 +208,8 @@ if __name__ == '__main__':
     # VARIABLES GLOBALES
     PATH = 'appels.csv'
     DEST = 'data.csv'
-    CONTENUTXT = charger(PATH)
-    LISTEDESAPPELS = analyser(CONTENUTXT)
-    etendre(LISTEDESAPPELS)
-    RESULTAT = backtotext(LISTEDESAPPELS)
-    enregistrer(RESULTAT, DEST)
+    contenutexte = charger(PATH)
+    listedesappels = analyser(contenutexte)
+    listedesappels = etendre(listedesappels)
+    contenutexte = backtotext(listedesappels)
+    enregistrer(contenutexte, DEST)
